73f6d1b0416c2e6e8f90dd84fc126affb0798e69
Pedro Meireles <pedro.carvalho@prevision.com.br>
2024-03-09 17:55:18 -0300

    configs

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template lang="pug">
 div
   h1 {{ $t('Welcome') }}
+  ColorToggle
 </template>
+
+
+<script setup lang="ts">
+import ColorToggle from './components/commons/ColorToggle.vue'
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/eslint-module', '@nuxtjs/i18n', 'shadcn-nuxt'],
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/color-mode', '@nuxtjs/eslint-module', '@nuxtjs/i18n', 'shadcn-nuxt'],
   i18n: {
     vuei18n: './i18n.config.ts',
     locales: ['pt', 'en'],
     defaultLocale: 'pt',
+  },
+  colorMode: {
+    classSuffix: ''
   },
   shadcn: {
     /**
@@ -17,5 +20,5 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui'
-  }
+  },
 })
