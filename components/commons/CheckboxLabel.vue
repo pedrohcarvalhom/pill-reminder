4c8ea695b716df07e9502ef13edba304ff0f948b
Pedro Meireles <pedro.carvalho@prevision.com.br>
2024-03-10 13:27:21 -0300

    finished schedule

---
+++ components/commons/CheckboxLabel.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+label(class="text-red-500 font-bold" :for="id.toString()") {{ props.label }}
+Checkbox(:id="id.toString()" class="rounded-full")
+</template>
+<script setup lang="ts">
+import { Checkbox } from '../ui/checkbox';
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: [Number],
+    required: true
+  }
+})
+</script>
--- components/pills/Schedule.vue
+++ components/pills/Schedule.vue
@@ -1,17 +1,24 @@
 <template lang="pug">
 Card(class="w-full bg-gray-100 shadow-lg")
   CardContent(class="flex flex-col py-2")
-    div(class="flex justify-between my-3")
-      label(class="text-red-500 font-bold"  for="terms2") 10:00 AM
-      Checkbox(id="terms2" class="rounded-full")
-    div(class="flex justify-between my-3")
-      label(class="text-red-500 font-bold" for="terms2") 12:00 AM
-      Checkbox(id="terms2" class="rounded-full")
-    div(class="flex justify-between my-3")
-      label(class="text-red-500 font-bold" for="terms2") 14:00 AM
-      Checkbox(id="terms2" class="rounded-full")
+    div(v-for="hour in hours" :key="hour.id" class="flex justify-between my-3")
+      checkbox-label(:id="hour.id" :label="hour.label")
 </template>
 
 <script setup lang="ts">
-import { Checkbox } from '../ui/checkbox';
+import CheckboxLabel from '../commons/CheckboxLabel.vue';
+const hours = ref([
+  {
+    id: 1,
+    label: '10:00 AM'
+  },
+  {
+    id: 2,
+    label: '12:00 AM'
+  },
+  {
+    id: 3,
+    label: '14:00 PM'
+  }
+])
 </script>
