96b4b356f9f20f6d5bda16681a85a4a1855d9f35
Pedro Meireles <pedro.carvalho@prevision.com.br>
2024-03-10 11:56:19 -0300

    change default font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/color-mode', '@nuxtjs/eslint-module', '@nuxtjs/i18n', 'shadcn-nuxt', 'nuxt-icon'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@nuxtjs/color-mode',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/i18n',
+    'shadcn-nuxt',
+    'nuxt-icon',
+  ],
   i18n: {
-    vuei18n: './i18n.config.ts',
+    vueI18n: './i18n.config.ts',
     locales: ['pt', 'en'],
     defaultLocale: 'pt',
   },
