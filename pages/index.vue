86dfbd89592d2de928b581cb294ebd4433780f88
pedrohcarvalhom <pedro.carvalho@prevision.com.br>
2024-03-11 23:40:32 -0300

    some changes and added form in schedule

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@ main
   dates-list
   hr.my-4
   div(class="flex justify-between items-center")
-    span(class="text-2xl ml-4") {{ $t('medicines.toTake') }}
-    a(class="mr-6 mt-1 text-red-500" @click="isRegisteringPill = !isRegisteringPill")
+    span(class="text-2xl ml-4 font-bold") {{ $t('medicines.toTake') }}
+    a(class="mr-6 mt-1 text-red-500 font-bold" @click="isRegisteringPill = !isRegisteringPill")
       icon(class="w-5 h-5 mr-1"  name="ion:add-circle")
       span {{ $t('buttons.add') }}
   medicines-list.mt-4
--- pages/pills/[id].vue
+++ pages/pills/[id].vue
@@ -14,10 +14,25 @@ main
     div(class="flex gap-2 mb-4")
       pills-measure
       pills-time
-    span(class="text-lg font-bold text-black dark:text-white") {{ $t('pills.schedule') }}
+    div(class="flex justify-between items-center my-4")
+      span(class="text-2xl font-bold text-black dark:text-white") {{ $t('pills.schedule') }}
+      a(class="flex items-center justify-center transition-all duration-150 ease-in-out" @click="editing = true")
+        icon(class="text-red-500 mb-1 mr-1 w-5 h-5" :class="{'animate-bounce': editing}"  name="mdi:pencil-plus-outline" )
+        span(class="text-lg font-bold mr-1 text-red-500 dark:text-red-300" :class="{'animate-bounce': editing}") {{ editing ? 'Editando...' : $t('buttons.edit') }}
     div.mt-2
+      div(v-show="editing" class="flex items-center gap-2")
+        Input(v-model="timeSelected" type="time" class="my-2 ml-auto w-40 bg-red-50")
+        div
+          a
+            Icon(name="icon-park-solid:check-one" class="w-8 h-8 transition-all duration-150 ease-in-out" :class="timeSelected ? 'text-green-500' : 'text-gray-400'")
+          a(@click="editing = false")
+            Icon(name="icon-park-solid:close-one" class="w-8 h-8 text-red-500")
       pills-schedule
 </template>
 
 <script setup lang="ts">
+import { Input } from '~/components/ui/input';
+
+const editing = ref(false);
+const timeSelected = ref("");
 </script>
